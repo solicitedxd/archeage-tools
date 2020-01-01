ff5d80aea12a10d5ed0f007b43bb5e4cbe957f5a
Mokulu <jsiebert9@icloud.com>
2019-12-31 23:29:50 -0800

    Convert Discord button to stateless component.

--- src/components/DiscordButton/DiscordButton.jsx
+++ src/components/DiscordButton/DiscordButton.jsx
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import DiscordLogo from './Discord-Logo-White.svg';
 
-class DiscordButton extends Component {
-  render() {
-    return (
-      <a href="https://discord.gg/5acTKYu" className="discord-button" target="_blank">
-        <div className="discord-icon">
-          <img src={DiscordLogo} alt="" />
-        </div>
-        <p className="discord-text">
-          Mokulu.io Discord
-        </p>
-      </a>
-    );
-  }
-}
+const DiscordButton = () => (
+  <a href="https://discord.gg/5acTKYu" className="discord-button" target="_blank">
+    <div className="discord-icon">
+      <img src={DiscordLogo} alt="" />
+    </div>
+    <p className="discord-text">
+      Mokulu.io Discord
+    </p>
+  </a>
+);
 
 export default DiscordButton;
