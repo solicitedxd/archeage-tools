57de3bde2ee550ef7c5d840a679f8dfafc28701f
Mokulu <jsiebert9@icloud.com>
2020-03-12 15:49:22 -0700

    Updated news styling and layout for mobile.

--- src/components/pages/Home/NewsPost.jsx
+++ src/components/pages/Home/NewsPost.jsx
@@ -13,12 +13,14 @@ import CreateIcon from '@material-ui/icons/Edit';
 import { Skeleton } from '@material-ui/lab';
 import { push } from 'actions/navigate';
 import { setNotification } from 'actions/notification';
+import cn from 'classnames';
 import Comments from 'components/Comments/Comments';
 import DraftJSRender from 'components/DraftJSRender';
 import IfPerm from 'components/IfPerm/IfPerm';
 import Link from 'components/Link';
 import OptionalTooltip from 'components/OptionalTooltip';
 import EditNewsPost from 'components/pages/Home/EditNewsPost';
+import Username from 'components/Username';
 import config from 'config';
 import { NOTIFICATION_TYPE } from 'constants/notification';
 import moment from 'moment';
@@ -68,39 +70,46 @@ class NewsPost extends Component {
   };
 
   render() {
-    const { match: { params: { postId, action } } } = this.props;
+    const { match: { params: { postId, action } }, mobile } = this.props;
 
     if (action === 'edit') {
       return <EditNewsPost {...this.props} />;
     }
 
     const { id, title, body, author, createDate, editDate, comments, commentCount, loading } = (this.state || this.props);
-
     const isEdited = (editDate && editDate !== createDate);
+    const standalone = postId !== null;
+
+    const dateBlock = loading
+      ? <Skeleton variant="text" width={60} />
+      : (
+        <OptionalTooltip
+          title={isEdited ? `Edited: ${new Date(editDate).toLocaleString(navigator.language || 'en-US')}` : null}
+        >
+          <Typography variant="overline" className={isEdited ? 'mark-tooltip' : ''}>
+            {moment(createDate).format('MMM DD, YYYY')}
+          </Typography>
+        </OptionalTooltip>
+      );
+
+    const authorBlock = loading
+      ? <Skeleton variant="text" width={60} className={cn({ 'title-text': standalone })} />
+      : <Typography variant="subtitle2" className={cn({ 'title-text': standalone, 'news-author': !mobile })}>
+        {mobile && 'Written '} by {mobile ? <Username user={author} /> : author}
+      </Typography>;
 
     return (
       <>
         <div className="section">
           <AppBar position="static">
             <Toolbar variant="dense">
-              <Typography variant="h5">
+              <Typography variant="h5" className={cn({ 'title-text': mobile })}>
                 {loading
                   ? <Skeleton variant="text" width={180} />
-                  : (postId !== null ? title : <Link to={`/news/${id}`} color="inherit">{title}</Link>)}
+                  : (standalone ? title : <Link to={`/news/${id}`} color="inherit">{title}</Link>)}
               </Typography>
-              <Typography variant="subtitle2" className="news-author title-text">
-                {loading
-                  ? <Skeleton variant="text" width={60} />
-                  : <>&nbsp;by {author}</>}</Typography>
-              {!loading &&
-              <OptionalTooltip
-                title={isEdited ? `Edited: ${new Date(editDate).toLocaleString(navigator.language || 'en-US')}` : null}
-              >
-                <Typography variant="overline" className={isEdited ? 'mark-tooltip' : ''}>
-                  {moment(createDate).format('MMM DD, YYYY')}
-                </Typography>
-              </OptionalTooltip>}
-              {loading && <Skeleton variant="text" width={60} />}
+              {!mobile && authorBlock}
+              {!mobile && dateBlock}
               {!loading &&
               <IfPerm permission="news.edit">
                 <Tooltip title="Edit Post">
@@ -114,14 +123,25 @@ class NewsPost extends Component {
           {!loading &&
           <Paper>
             <DraftJSRender contentState={stringToContentState(body)} />
-            {postId === null &&
-            <CardActions className="comments-action" disableSpacing>
+            {(!standalone || mobile) &&
+            <CardActions className="paper-action" disableSpacing>
+              {!standalone &&
               <Button
                 startIcon={<ChatBubbleIcon />}
                 onClick={() => push(`/news/${id}#comments`)}
               >
-                Comments: {commentCount}
-              </Button>
+                {commentCount}{!mobile && ' Comments'}
+              </Button>}
+              {mobile &&
+              (standalone
+                ? <>
+                  {authorBlock}
+                  {dateBlock}
+                </>
+                : <div style={{ textAlign: 'right' }}>
+                  {authorBlock}
+                  {dateBlock}
+                </div>)}
             </CardActions>}
           </Paper>}
           {loading &&
@@ -131,7 +151,7 @@ class NewsPost extends Component {
             </div>
           </Paper>}
         </div>
-        {postId !== null && id && !loading &&
+        {standalone && id && !loading &&
         <Comments
           postId={`NEWS-${id}`}
           comments={comments}
@@ -142,8 +162,12 @@ class NewsPost extends Component {
   }
 }
 
+const mapStateToProps = ({ display: { mobile } }) => ({
+  mobile,
+});
+
 const mapDispatchToProps = {
   setNotification,
 };
 
-export default connect(null, mapDispatchToProps)(NewsPost);
+export default connect(mapStateToProps, mapDispatchToProps)(NewsPost);
