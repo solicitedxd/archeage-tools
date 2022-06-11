be05ca4d05c5b01a7ecd835f062e0898a18d87e4
Fernando Gonçalves Ferreira Oliveira <fernando.oliveira@solutis.com.br>
2022-06-11 18:00:12 -0300

    update

--- src/components/DiscordButton/DiscordButton.jsx
+++ src/components/DiscordButton/DiscordButton.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import DiscordLogo from './Discord-Logo-White.svg';
 
 const DiscordButton = () => (
-  <a href="https://discord.gg/5acTKYu" className="discord-button" target="_blank" rel="noreferrer">
+  <a href="https://discord.gg/link" className="discord-button" target="_blank" rel="noreferrer">
     <div className="discord-icon">
       <img src={DiscordLogo} alt="" />
     </div>
     <p className="discord-text">
-      Mokulu.io Discord
+      Nop
     </p>
   </a>
 );
--- src/components/DonateButton/DonateButton.jsx
+++ src/components/DonateButton/DonateButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DonateButton = () => (
   <a
-    href="https://www.paypal.com/donate?hosted_button_id=JCJQRMEBK2MF8"
+    href="https://www.paypal.com/donate/?business=MZVFQ38LFC8EY&no_recurring=0&item_name=Ajudar+a+manter+o+servi%C3%A7o+funcionando+e+atualizado.%0AHelping+the+service+stay+up+and+update&currency_code=BRL"
     className="donate-button"
     target="_blank"
     rel="noreferrer"
