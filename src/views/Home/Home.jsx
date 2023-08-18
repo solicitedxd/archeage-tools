b654b63e2a51580aa005601367903eba72699393
solicitedxd <142409492+solicitedxd@users.noreply.github.com>
2023-08-18 09:10:24 -0500

    Update Home.jsx

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -35,22 +35,14 @@ class Home extends Component {
   state = {
       posts: [{"id":  1,
                "title":  "Welcome to ArcheRage!",
-               "body":  "The official Mokulu is no longer up, "+
-               "but he left a big part of his legacy available, which is very good! \n\n " +
-               "As our server has customized events I decided to invest a part of my time "+
-               "to facilitate the daily life of the new players that i am also a part of.\n\n"+
-               "\n\n"+
-               "The idea is to make the schedules part work, i have no experience with react, "+
-               "and by the way it took me a while to upload this to heroku. "+
-               "My job now is to map event times.\n\n" +
+               "body":  "Mokulo's ArcheAge schedule, adapted for ArcheRage by Dyani\n\n " +
+               "All credit for the original site to Mokulo and the ArcheRage edits to Dyani\n\n"+
+               "\n\n"
 
-               "I'll be honest, I'm doing something very basic, but if you still like it "+
-               "and it's useful, I invite you to <b>donate</b> ❤ thanks!\n"+
-                   "Made by Mokulu and reused by ' <b>Dyani</b> ' ( in game nick ) 🐱 " +
-               "Forgive me for my basic english of 'google translator'...\n\n\n\n"+
-               "<img src='https://github.com/fernandogfo/archeage-tools/blob/data/static/images/leprechaun.png?raw=true'>",
-               "author":  "Dyani",
-               "createDate":  "06/10/2022",
+               You can contact me on Discord at "Solicited" if you believe there to be any errors\n"+
+     
+               "author":  "Solicited",
+               "createDate":  "08/18/2023",
                "editDate":  "",
                "comments":  "teste",
                "commentCount":  0,
